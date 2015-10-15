2f6a8c35c5c02b3d37cb7ced1f1bc5634d13b4c0
Armin Vogt <avogt@s-und-n.de>
2015-10-15 08:00:13 +0200

    clean up, new ENV disk_cache_mb, default is 500mb

--- Dockerfile
+++ Dockerfile
@@ -3,32 +3,28 @@ MAINTAINER Armin Vogt avogt@s-und-n.de
 
 # http://wiki.alpinelinux.org/wiki/Setting_up_Transparent_Squid_Proxy
 #
-
-RUN set -x && export http_proxy="http://172.17.42.1:3128"  \
+ENV http_proxy="http://172.17.42.1:3128"  
+RUN set -x  \
  && mkdir -p /srv/openldap.d /etc/openldap/sasl2 \
- && apk add --update curl gettext acf-squid bash gnutls-utils
+ && apk add --update curl gettext acf-squid
 
 ENV SQUID_VERSION=3.3.8 \
     SQUID_CACHE_DIR=/var/spool/squid \
     SQUID_LOG_DIR=/var/log/squid \
     SQUID_USER=squid \
-	CA_CERT_PASSWORD=toor \
-	CA_DOMAIN=s-und-n.de
-
+	proxy_pass= \
+	proxy_user= \
+	parent_proxy=10.10.1.102 \
+	https_parent_proxy= \
+	parent_port=80 \
+	disk_cache_mb=500
 
 COPY squid.conf /etc/squid/squid.conf
-COPY createCA.sh /
-ENV proxy_pass=
-ENV proxy_user=
-ENV parent_proxy=10.10.1.102
-ENV https_parent_proxy=
-ENV parent_port=80
 
 
 COPY entrypoint.sh /sbin/entrypoint.sh
 RUN chmod 755 /sbin/entrypoint.sh
 
-VOLUME ["/out"]
 EXPOSE 3128
 ENTRYPOINT ["/sbin/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # arguments come from the commandline (when using the makefile: make RUN_ARGS="1 2 3" comes as $1,$2,$3 vars)
 #
 echo "called with arguments:" \"$1\"  \"$2\" \"$3\"
@@ -8,100 +8,23 @@ set -e
 set |grep SQUID
 find /etc/squid*
 echo "squid bin: $(which squid)"
-echo "ssl_crtd bin: $(which ssl_crtd)"
-export myCA_pem=/myCA.pem
-
-#########################################################################################################
-setup_gnutls() {
-	cat > cert.cfg <<EOF 
-# X.509 Certificate options
-#
-# DN options
-
-# The organization of the subject.
-organization = "S&N AG"
-
-# The organizational unit of the subject.
-unit = "Docker Works"
-
-# The locality of the subject.
-locality = "OWL"
-
-# The state of the certificate owner.
-state = "NRW"
-
-# The country of the subject. Two letter code.
-country = DE
-
-# The common name of the certificate owner.
-cn = "Armin Vogt"
-
-# A user id of the certificate owner.
-uid = "avogt"
-# The serial number of the certificate
-# Comment the field for a time-based serial number.
-serial = 007001
-
-# In how many days, counting from today, this certificate will expire.
-# Use -1 if there is no expiration date.
-expiration_days = 700
-
-# An email in case of a person
-email = "avogt@s-und-n.de"
-
-# Challenge password used in certificate requests
-challenge_password = 123456
-# Path length contraint. Sets the maximum number of
-# certificates that can be used to certify this certificate.
-# (i.e. the certificate chain length)
-#path_len = -1
-path_len = 2
-
-# An URL that has CRLs (certificate revocation lists)
-# available. Needed in CA certificates.
-crl_dist_points = "http://www.getcrl.crl/getcrl/"
-# Whether this is a CA certificate or not
-ca 
-
-# Whether this key will be used to sign other certificates. The
-# keyCertSign flag in RFC5280 terminology.
-cert_signing_key
-
-# Whether this key will be used to sign CRLs. The
-# cRLSign flag in RFC5280 terminology.
-crl_signing_key
-
-EOF
-}
-
-create_certs() {
-	certtool --generate-privkey --outfile ca-key.pem
-	certtool --generate-self-signed --load-privkey ca-key.pem --outfile $myCA_pem --template cert.cfg
-	
-	FILE="myCA.der"
-	openssl x509 -in myCA.pem -outform DER -out /out/$FILE
-	echo "wrote $FILE to /out"
-}
 
 
 envsubstitution() {
   export proxy_pass
   export proxy_user
-  export corporate_proxy
+  export parent_proxy
+  export disk_cache_mb
+  
   if [[ -z "$proxy_user" ]]; then
 	echo "removing login from squid conf since ENV proxy_user is empty"
 	sed -i s/.login=.*//g /etc/squid/squid.conf
   fi
   cat /etc/squid/squid.conf | envsubst > tmp_squid.conf && mv tmp_squid.conf /etc/squid/squid.conf 
-  echo "--------setup squid.conf:"
+  echo "Disk cache (-e disk_cache_mb=<mb>) to be used is set to: "$disk_cache_mb" megabytes"
   cat /etc/squid/squid.conf
-  echo "--------end of squid.conf"
 }
-create_ssl_cache() {
-	mkdir -p /var/lib/ssl_db
-	$(which ssl_crtd) -c -s /var/lib/ssl_db
-	chown squid:squid -R /var/lib/ssl_db
-}
+
 create_log_dir() {
   mkdir -p ${SQUID_LOG_DIR}
   chmod -R 755 ${SQUID_LOG_DIR}
@@ -116,16 +39,6 @@ create_cache_dir() {
 create_log_dir
 create_cache_dir
 envsubstitution
-
-# allow arguments to be passed to squid3
-if [[ ${1} == "create-certs" || ${2} == "create-certs" ]]; then
-	echo "you asked me to create a new CA. Complying.."
-	EXTRA_ARGS="${@:2}"
-	set --
-	setup_gnutls
-	create_certs
-	exec "bash"
-fi
 
 # default behaviour is to launch squid
 if [[ -z ${1} ]]; then
